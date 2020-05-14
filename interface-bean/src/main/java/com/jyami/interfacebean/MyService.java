72b111ec50977725aaa281f7a791fc73b28d4394
mjung1798 <mor2222@naver.com>
2020-05-15 01:14:24 +0900

    Feat: InterfaceBean

---
+++ interface-bean/src/main/java/com/jyami/interfacebean/MyService.java
@@ -0,0 +1,8 @@
+package com.jyami.interfacebean;
+
+/**
+ * Created by jyami on 2020/04/27
+ */
+public interface MyService {
+    void doSomething();
+}
---
+++ interface-bean/src/test/java/com/jyami/interfacebean/InterfaceBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyami.interfacebean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterfaceBeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
