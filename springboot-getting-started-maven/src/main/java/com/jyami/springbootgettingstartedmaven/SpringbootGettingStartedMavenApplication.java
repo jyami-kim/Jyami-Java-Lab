9c340125b141b5644bddf2d6c1d7d1fdd7faaf02
mjung1798 <mor2222@naver.com>
2020-05-17 20:54:56 +0900

    Feat: https connector 실습
    https ssl 을 생성하고,
    내 서버를 http, https 둘다로 받을 수 있게 connector 추가하기:

--- springboot-getting-started-maven/src/main/java/com/jyami/springbootgettingstartedmaven/SpringbootGettingStartedMavenApplication.java
+++ springboot-getting-started-maven/src/main/java/com/jyami/springbootgettingstartedmaven/SpringbootGettingStartedMavenApplication.java
@@ -1,10 +1,35 @@
 package com.jyami.springbootgettingstartedmaven;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootGettingStartedMavenApplication {
+
+	@GetMapping("hello")
+	public String hello(){
+		return "hello Spring~";
+	}
+
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+		return tomcat;
+	}
+
+	private Connector createStandardConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setPort(8080);
+		return connector;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootGettingStartedMavenApplication.class, args);
