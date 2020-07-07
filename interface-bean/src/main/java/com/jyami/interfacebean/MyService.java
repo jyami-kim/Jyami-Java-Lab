4331984e127b298f78e3afb5d52348300d87c307
mjung1798 <mor2222@naver.com>
2020-07-07 11:39:43 +0900

    Feat: Delete and make project folder
    1. delete interface-bean project
    2. create spring example forlder
    3. practice springboot lecture example project

--- interface-bean/src/main/java/com/jyami/interfacebean/MyService.java
+++
@@ -1,8 +0,0 @@
-package com.jyami.interfacebean;
-
-/**
- * Created by jyami on 2020/04/27
- */
-public interface MyService {
-    void doSomething();
-}
--- springboot-getting-started-maven/src/test/java/com/jyami/springbootgettingstartedmaven/SpringbootGettingStartedMavenApplicationTests.java
+++ springboot-getting-started-maven/src/test/java/com/jyami/springbootgettingstartedmaven/SpringbootGettingStartedMavenApplicationTests.java
@@ -1,13 +1,21 @@
 package com.jyami.springbootgettingstartedmaven;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SpringbootGettingStartedMavenApplicationTests {
 
+	@Autowired
+	Environment environment;
+
 	@Test
 	void contextLoads() {
+		assertThat(environment.getProperty("jyami.name")).isEqualTo("devLog");
 	}
 
 }
