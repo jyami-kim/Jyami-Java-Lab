23af67d5c32015eb2ac43842d599db0be596341f
mjung1798 <mor2222@naver.com>
2020-07-13 17:02:07 +0900

    Feat: controller test - env random-port

--- springboot-getting-started-maven/src/test/java/com/jyami/springbootgettingstartedmaven/testing/SampleControllerTest.java
+++ springboot-getting-started-maven/src/test/java/com/jyami/springbootgettingstartedmaven/testing/SampleControllerTest.java
@@ -1,34 +1,37 @@
 package com.jyami.springbootgettingstartedmaven.testing;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by jyami on 2020/07/13
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@AutoConfigureMockMvc
-class SampleControllerTest {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SampleControllerTest {
+
     @Autowired
-    MockMvc mockMvc;
+    private TestRestTemplate testRestTemplate;
+
+    @MockBean
+    private SampleService sampleService;
 
     @Test
-    void testHello() throws Exception {
-        mockMvc.perform(get("/helloName"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("hello jyami"))
-                .andDo(print());
+    public void name() {
+
+        when(sampleService.getName()).thenReturn("minjeong");
+
+        String forObject = testRestTemplate.getForObject("/helloName", String.class);
+        assertThat(forObject).isEqualTo("hello minjeong");
+
     }
 }
