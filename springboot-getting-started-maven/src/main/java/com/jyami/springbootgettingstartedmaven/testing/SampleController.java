37218770d697372bbf8b485aa6de255fb3b27c4a
mjung1798 <mor2222@naver.com>
2020-07-13 17:24:54 +0900

    Feat: test log : outputCapture(but deprecated)

--- springboot-getting-started-maven/src/main/java/com/jyami/springbootgettingstartedmaven/testing/SampleController.java
+++ springboot-getting-started-maven/src/main/java/com/jyami/springbootgettingstartedmaven/testing/SampleController.java
@@ -1,5 +1,7 @@
 package com.jyami.springbootgettingstartedmaven.testing;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
 
+    Logger log = LoggerFactory.getLogger(SampleController.class);
+
     @Autowired
     private SampleService sampleService;
 
     @GetMapping("/helloName")
     public String hello() {
+        log.info("jyami minjeong");
+        System.out.println("hello~~");
         return "hello " + sampleService.getName();
     }
 
--- springboot-getting-started-maven/src/test/java/com/jyami/springbootgettingstartedmaven/testing/SampleControllerTest.java
+++ springboot-getting-started-maven/src/test/java/com/jyami/springbootgettingstartedmaven/testing/SampleControllerTest.java
@@ -1,40 +1,51 @@
 package com.jyami.springbootgettingstartedmaven.testing;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.rule.OutputCapture;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by jyami on 2020/07/13
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest(SampleController.class)
 public class SampleControllerTest {
 
     @Autowired
-    private WebTestClient webTestClient;
+    MockMvc mockMvc;
 
     @MockBean
-    private SampleService sampleService;
+    SampleService sampleService;
+
+    @Rule
+    public OutputCapture outputCapture = new OutputCapture();
+
 
     @Test
-    public void name() {
+    public void name() throws Exception {
 
         when(sampleService.getName()).thenReturn("minjeong");
 
-        webTestClient.get().uri("/helloName")
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(String.class).isEqualTo("hello minjeong");
+        mockMvc.perform(get("/helloName"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello minjeong"))
+                .andDo(print());
+
+        assertThat(outputCapture.toString())
+                .contains("jyami minjeong")
+                .contains("hello~~");
     }
 }
