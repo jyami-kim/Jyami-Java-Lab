1f40697b1c090d2318465b9ec12e3b05f7f51af8
mjung1798 <mor2222@naver.com>
2020-07-16 20:54:06 +0900

    Feat: CORS Mapping

--- springboot-getting-started-maven/src/main/java/com/jyami/springbootgettingstartedmaven/webMVC/config/WebConfig.java
+++ springboot-getting-started-maven/src/main/java/com/jyami/springbootgettingstartedmaven/webMVC/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.jyami.springbootgettingstartedmaven.webMVC.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +16,11 @@ public class WebConfig  implements WebMvcConfigurer {
         registry.addResourceHandler("/m/**")
                 .addResourceLocations("classpaht:/m/")
                 .setCachePeriod(2);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:10080");
     }
 }
