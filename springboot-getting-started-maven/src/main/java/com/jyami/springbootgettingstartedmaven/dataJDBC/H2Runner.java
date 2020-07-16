9fc4295da3e37473102fea559f03e569c6e6c610
mjung1798 <mor2222@naver.com>
2020-07-16 21:53:05 +0900

    Feat: spring in-memory DB

---
+++ springboot-getting-started-maven/src/main/java/com/jyami/springbootgettingstartedmaven/dataJDBC/H2Runner.java
@@ -0,0 +1,38 @@
+package com.jyami.springbootgettingstartedmaven.dataJDBC;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.Statement;
+
+/**
+ * Created by jyami on 2020/07/16
+ */
+@Component
+public class H2Runner implements ApplicationRunner {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        try(Connection connection = dataSource.getConnection()){
+            System.out.println(connection.getMetaData().getURL());
+            System.out.println(connection.getMetaData().getUserName());
+
+            Statement statement = connection.createStatement();
+            String sql = "CREATE TABLE USER (ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY (id));";
+            statement.executeUpdate(sql);
+        }
+
+        jdbcTemplate.execute("INSERT INTO USER VALUES (1, 'jyami')");
+    }
+}
