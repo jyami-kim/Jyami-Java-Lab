888c4ef7eccec6c19627b5c507206e63c3c6b6d0
mjung1798 <mor2222@naver.com>
2020-07-22 00:57:46 +0900

    Feat: restClient custom

---
+++ springboot-getting-started-maven/src/main/java/com/jyami/springbootgettingstartedmaven/restClient/RestClientConfiguration.java
@@ -0,0 +1,35 @@
+package com.jyami.springbootgettingstartedmaven.restClient;
+
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+/**
+ * Created by jyami on 2020/07/22
+ */
+@Configuration
+public class RestClientConfiguration {
+    @Bean
+    public WebClientCustomizer webClientCustomizer(){
+        return new WebClientCustomizer() {
+            @Override
+            public void customize(WebClient.Builder webClientBuilder) {
+                webClientBuilder.baseUrl("http://localhost:8080");
+            }
+        };
+    }
+
+    @Bean
+    public RestTemplateCustomizer restTemplateCustomizer(){
+        return new RestTemplateCustomizer() {
+            @Override
+            public void customize(RestTemplate restTemplate) {
+                restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+            }
+        };
+    }
+}
