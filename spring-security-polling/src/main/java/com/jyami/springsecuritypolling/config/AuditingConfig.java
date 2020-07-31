88b3341c3d6531f4695cf8b2157fef4a8d0a64a7
mjung1798 <mor2222@naver.com>
2020-07-31 22:23:09 +0900

    Feat: auditing config setting -for updateby, createby

--- spring-security-polling/src/main/java/com/jyami/springsecuritypolling/config/AuditingConfig.java
+++ spring-security-polling/src/main/java/com/jyami/springsecuritypolling/config/AuditingConfig.java
@@ -1,7 +1,15 @@
 package com.jyami.springsecuritypolling.config;
 
+import com.jyami.springsecuritypolling.security.UserPrincipal;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
 
 /**
  * Created by jyami on 2020/07/30
@@ -9,4 +17,25 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @Configuration
 @EnableJpaAuditing
 public class AuditingConfig {
+
+    @Bean
+    AuditorAware<Long> auditorProvider(){
+        return new SpringSecurityAuditAwareImpl();
+    }
+
+}
+
+class SpringSecurityAuditAwareImpl implements AuditorAware<Long>{
+
+    @Override
+    public Optional<Long> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication == null || !authentication.isAuthenticated()
+                || authentication instanceof AnonymousAuthenticationToken)
+            return Optional.empty();
+
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+        return Optional.ofNullable(userPrincipal.getId());
+    }
 }
